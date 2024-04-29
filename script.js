61dca9f6841faf2fcd2f7b942827381d19076351
Ana <74693547+itsmeanaaa@users.noreply.github.com>
2024-04-30 01:41:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-function makeWish() {
-  var name = document.getElementById('name').value;
-  var age = document.getElementById('age').value;
-
-  if (name && age) {
-    alert("Happy " + age + "th Birthday, " + name + "!");
-    document.getElementById('flame').style.visibility = "visible";
-    setTimeout(function() {
-      document.getElementById('flame').style.visibility = "hidden";
-    }, 1000);
-  } else {
-    alert("Please enter your name and age.");
-  }
-}
+const cake = document.getElementById("cake");
+cake.addEventListener('click', () => {
+    alert("Wishing you very happy happy birthday!");
+  cake.classList.toggle('blow');
+});
